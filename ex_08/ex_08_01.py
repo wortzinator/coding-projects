f1bf908acda75e791522e320a6f984c019e6e651
Nathan Wertz <nathan@Nathans-MacBook-Pro.local>
2022-09-26 19:33:25 -0400

    updated

--- ex_08/ex_08_01.py
+++ ex_08/ex_08_01.py
@@ -1,9 +1,11 @@
 fname = input("Enter file name: ")
 fh = open(fname)
-lst = list()
-for line in fh:
-    print(line.rstrip())
-
+lst = list(fh)
+for line in lst:
+    line = line.strip()
+    lines = line.split()
+    print(lines)
+    #print(lines.split())
 
 
 
