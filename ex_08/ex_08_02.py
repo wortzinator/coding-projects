35ce3e7645b260c2e32da4a19f72cb72af803ba6
Nathan Wertz <nathan@Nathans-MacBook-Pro.local>
2022-09-27 03:24:52 -0400

    as is, the following formula prints the from line, just need to split somehow and print the email.

--- ex_08/ex_08_02.py
+++ ex_08/ex_08_02.py
@@ -1,8 +1,19 @@
-fname = input("Enter file name: ")
-if len(fname) < 1:
-    fname = "mbox-short.txt"
-
-fh = open(fname)
 count = 0
+
+fname = input("Enter file name: ")
+hname = open(fname)
+#sline = hname.read()
+#lin = hname.split()
+for line in hname:
+    line = line.rstrip()
+    if line.startswith('From '):
+        print(line)
+        count = count + 1
+    if not line.startswith('From ') :
+        continue
+
+
+#print(line)
+
 
 print("There were", count, "lines in the file with From as the first word")
