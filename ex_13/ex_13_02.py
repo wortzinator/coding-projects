663f42ed4a80d0d81444f2691878ae473fd313b3
Nathan Wertz <lnkwab1025@gmail.com>
2022-10-20 01:04:47 -0400

    finished ex_13_02

--- ex_13/ex_13_02.py
+++ ex_13/ex_13_02.py
@@ -1,19 +1,33 @@
 import urllib.request, urllib.parse, urllib.error
+from urllib.request import urlopen
 import json
 import ssl
 
+# Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter URL: ')
-webinfo = urllib.request.urlopen(url)
-web = webinfo.read().decode()
+#user input
+url = input('Unter URL: ')
+webinfo = urllib.request.urlopen(url).read().decode()
 
+#convert to json
+data = json.loads(webinfo)
 
-info = json.loads(web)
-print('User count: ', len(info))
+count = 0
+value_list = []
+num_list = []
 
-for items in info:
-    print('Name', items['name'])
-    print('Count', items['count'])
+#extract the comments
+for a in range(len(data["comments"])): # 'a' is the deliminator, the 'range()' is the range of the comments which is 2
+    value = data["comments"][a] # value is the 'data' list "comments" 
+    value_list.append(value) # adding the value list to t he value_list dict
+
+#extracting the dictionary in list
+for item in value_list:
+    count += 1
+    number = int(item["count"])
+    num_list.append(number)
+print('Count', count)
+print('Sum', sum(num_list))
